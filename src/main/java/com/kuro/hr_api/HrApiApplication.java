dba3d06df06a7ff899f4c519f0ef53d0d4930bc1
Thibaut <rack.kendorim@gmail.com>
2024-07-04 15:06:25 +0200

    :tada: Minimal Project

---
+++ src/main/java/com/kuro/hr_api/HrApiApplication.java
@@ -0,0 +1,13 @@
+package com.kuro.hr_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrApiApplication.class, args);
+    }
+
+}
